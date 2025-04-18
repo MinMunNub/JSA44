9622f8f4aa654dbd6e0c20fb59b2bc8ecfbeb835
MinMunNub <vudinhtunganh@gmail.com>
2025-04-18 08:59:56 +0700

    baitapbuoi3

---
+++ bai3/day3.js
@@ -0,0 +1,193 @@
+        // // console.log() => cach de ta co the in ra gia tri nao day bang console
+        // console.log("hoc ve javascript");
+        // // kieu du lieu javascript 
+        // // chuoi(swing): chua duoc nhieu ky tu khac nhau va duoc de trong "" hoac '' va ``
+        // console.log(typeof"toi ten la tung bel");
+        // console.log("ble ble ble bla bla");
+        // console.log('"nguoi beo 5 tan"');
+        // console.log("'meo beo thich macdonalds'");
+        // console.log(`tungbelskibidi`);
+        // console.log("I"  +  " HATE"  +  " BLACK");
+        // // so(number):
+        // console.log("my skibidi is" + " 69")
+        // console.log(typeof-7749)
+        // console.log(6.9)
+        // true
+        // false
+        // (boolean)
+        // console.log(true)
+        // console.log(false)
+        // undefined
+        // null
+        // object
+
+        // cach de check kieu du lieu
+        // typeoff
+
+        // khai bao bien trong js
+        // bien la dai dien cho mot truong du lieu nao do
+        // cac ky tu de khai bao bien la let, var, const
+        // // cong thuc let/const/var ten_bien = gia tri nao do
+        // let address = "so 69 pho skibidi thanh pho sigma"
+        // // update gia tri cua bien
+        // address = "LA"
+        // console.log("i am at" + address + "it is very skibidi")
+        // camel case: viet hoa chu cai dau tu tu thu 2 tro di
+        // snake case: cac tu duoc ngan cach boi dau _
+
+        // address = " THPT LTT"
+        // console.log("Toi ten la vu dinh tung anh, toi nam nay" + " 16" + " Toi dang hoc o truong" + address + " toi co" + " 7" + " nguoi ban than")
+
+        // let name = "tunganh"
+        // let age = 16
+        // let schooladdress = "LTT"
+        // let member = 6
+        // console.log
+        // (`Toi ten la ${name}
+        //  toi nam nay ${age} tuoi
+        //  toi dang hoc o ${schooladdress}
+        //  toi la thanh vien so ${member}`)
+
+        // hang prompt trong js cho phep nguoi dung khai bao gia tri tu trinh duyet
+        // // ham alert("loi dan") => thong bao ra trinh duyet
+        // let input = prompt("enter something here")
+        // console.log("output: " + input)
+        // alert("minmunskibidi")
+        
+        // phep tinh trong js +, -, *, /, **
+
+        // let a = 20
+        // let b = 6
+
+        // let count = a + b;
+        // console.log("count =" + math.sqrt{36})
+
+        // let a = Number(prompt("enter a number"))
+        // if (a <=100) {
+        // let count = a * 2
+        // console.log("result: " + count)
+        // }
+
+
+        // let a = Number(prompt("enter a"))
+        // let b = Number(prompt("enter b"))
+        // let c = Number(prompt("enter c"))
+        // if (a <= 1000 && b <= 1000 && c <= 1000) {
+        // let count = (a - b) * c
+        // console.log("Result: " + count)
+        // }
+
+
+        // let a = Number(prompt("enter a"));
+        // let b = Number(prompt("enter b"));
+
+        // if (a <= 1000 && b <= 1000) {
+        // let count = (a + b + a * b);
+        // console.log("Result: " + count);
+        // }
+
+
+        // let a = Number(prompt("enter a"));
+        // let b = Number(prompt("enter b"));
+        // let c = Number(prompt("enter c"));
+
+        // if (a <= 100 && b <= 100 && c <= 100) {
+        //   let s = (a + b + c) / 2;
+        //   let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        //   console.log("Result: " + area.toFixed(2));
+        // }
+
+        // BTVN
+        // bai1
+        // let a = Number(prompt("Nhap mot so be hon hoac bang 100:"));
+
+        //  if (a <= 100) {
+        //  let result = a * 2;
+        //    console.log("Ket qua la:" + result);
+        //  }
+
+        // bai2
+        // let input = prompt("Nhap 3 so cach nhau bang dau cach be hon hoac bang 100");
+        // let [a, b, c] = input.split(" ").map(Number);
+        
+        // if (a <= 100 && b <= 100 && c <= 100) {
+        //   let result = (a - b) * c;
+        //   console.log("Ket qua la:" + result);
+        // }
+
+        // bai3
+        // let ch = prompt("Nhap mot chu in hoa");
+
+        // if (ch.length === 1 && ch >= 'A' && ch <= 'Z') {
+        //         let lower = ch.toLowerCase();
+        //         console.log("Ket qua la:" + lower);
+        // }
+
+        // bai4
+        // let n = Number(prompt("Nhap mot so n be hon hoac bang 50"));
+        // if (n <= 50) {
+        //   let count = 0;
+        //   for (let i = 1; i <= n; i++) {
+        //     if (i > (n - 1) / 2) {
+        //       count++;
+        // }
+        // }
+        // console.log("Ket qua la:" + count);
+        // }
+
+        // bai5
+        // let n = Number(prompt("Nhap mot so nguyen n be hon hoac bang 15"));
+        // if (n <= 15) {
+        //   let count = 0;
+        //   for (let i = n + 1; i < n * n; i++) {
+        //     count++;
+        //   }
+        //   console.log("Ket qua la:" + count);
+        // }
+
+        // bai6
+        // let str = prompt("Nhap mot chuoi ky tu viet lien ko dau");
+        // let reversed = str.split("").reverse().join("");
+        // console.log("Ket qua la:" + reversed);
+
+        // bai7
+        // let input = prompt("Nhap hai so be hon hoac bang 1000 cach nhau bang dau cach");
+        // let [a, b] = input.split(" ").map(Number);
+        
+        // if (a <= 1000 && b <= 1000) {
+        //   let result = a + b + (a * b);
+        //   console.log("Ket qua la:" + result);
+        // }
+
+        // bai8
+        // let ch = prompt("Nhap mot chu thuong");
+
+        // if (ch.length === 1 && ch >= 'a' && ch <= 'z') {
+        //   let upper = ch.toUpperCase();
+        //   console.log("Ket qua la:", upper);
+        // }
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+        
+
+
+
+
+
+
+
+
+        
