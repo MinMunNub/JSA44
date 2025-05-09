53fa60cf2bdd14d8662717d1c6a49ec539fb5ad1
MinMunNub <vudinhtunganh@gmail.com>
2025-05-09 20:30:35 +0700

    btbuoi6olop

---
+++ bai6/bai6.js
@@ -0,0 +1,109 @@
+/**
+ * vong for dung de thuc thi mot dong code lap di lap lai nhieu lan voi cung mot quy tac, logic
+ * for(vi tri khoi tao, dieu kien de vong lap chay, su thay doi cua vong lap) {
+ * 
+ * }
+ * 
+ */
+
+
+// for (let i = 0; i <= 100; i++) {
+//     if (i % 2 === 0 && (i+4) % 6 === 0 ) {
+//             console.log(i);
+//         }
+//     }
+
+// for (let i = 100; i >= 1; i--) {
+//     console.log(i);
+// }
+
+
+// for (let i = 100; i >= 50; i--) {
+//     console.log(i);
+// }
+
+
+// for (let i = 50; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// let kq = "";
+// for (let i = 1; i <= 10; i++) {
+//     kq += i + " ";
+// }
+// console.log("in ra 1 den 10 tren mot hang = " + kq.trim());
+
+// let a = 1;
+// let b = 5;
+// let kq = "";
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//     kq += i + " ";
+//     sum += i;
+// }
+
+// console.log("in ra cac so tu " + a + " den " + b + ": " + kq.trim());
+// console.log("in ra tong cac so tu " + a + " den " + b + ": " + sum);
+
+
+
+// let n = 10;
+// let kq = 1;
+
+// for (let i = 1; i <= n; i++) {
+//     kq = kq * i;
+// }
+
+// console.log("giai thua cua", n, "la:", kq);
+
+
+
+// let kq = "";
+
+// for (let i = 1; i <= 9; i++) {
+//     kq += i + " ";
+
+//     if (i % 3 === 0) {
+//         kq += "\n";
+//     }
+// }
+
+// console.log("kq\n" + kq);
+
+
+
+// let cars = ["Toyota", "Honda", "Kia", "Mazda", "Vinfast"];
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
